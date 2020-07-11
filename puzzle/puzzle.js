b288d3094e49c4a837df7a6e8c53767ab3ac2fda
songyiqun <594207613@qq.com>
2020-07-11 22:42:35 +0800

    20200711

---
+++ puzzle/puzzle.js
@@ -0,0 +1,88 @@
+﻿var time = 0;
+var status = 0;
+/* 游戏状态
+ * 0：未开始
+ * 1：进行中
+ * 2：暂停中
+ */
+var set_timer;
+var d = new Array(10);
+var nextpos;//相邻格子编号
+var realpos;//实际位置
+
+function move(id) {
+    if (status != 1) return;
+    var cur = 0;
+    for (let i = 1; i <= 9; ++i) if (d[i] == id) { cur = i; break; }
+    for (let i = 0; i < nextpos[cur].length; ++i) {
+        if (d[nextpos[cur][i]] == 0) {
+            let y = nextpos[cur][i];
+            document.getElementById("d" + id).style.left = realpos[y][0] + "px";
+            document.getElementById("d" + id).style.top = realpos[y][1] + "px";
+            d[cur] = 0; d[y] = id;
+            break;
+        }
+    }
+    var flag = true;
+    for (let i = 1; i <= 8; ++i) if (d[i] != i) { flag = false; break; }
+    if (flag == true) {
+        status = 0;
+        document.getElementById("start").innerHTML = "开始";
+        clearInterval(set_timer);
+        alert("Congratulation!");
+    }
+}
+
+function timer() {
+    time += 1;
+    var m = parseInt(time / 60);
+    var s = parseInt(time % 60);
+    document.getElementById("timer").innerHTML = m + "分" + s + "秒";
+}
+
+function start() {
+    if (status == 0) {
+        document.getElementById("start").innerHTML = "暂停";
+        status = 1;
+        set_timer = setInterval(timer, 1000);
+        reset();
+    }
+    else if (status == 1) {
+        document.getElementById("start").innerHTML = "开始";
+        status = 2;
+        clearInterval(set_timer);
+    }
+    else if (status == 2) {
+        document.getElementById("start").innerHTML = "暂停";
+        status = 1;
+        set_timer = setInterval(timer, 1000);
+    }
+}
+
+function reset() {
+    time = 0;
+    document.getElementById("timer").innerHTML = 0 + "分" + 0 + "秒";
+    nextpos = new Array(
+        [0], [2, 4], [1, 3, 5], [2, 6], [1, 5, 7], [2, 4, 6, 8], [3, 5, 9], [4, 8], [5, 7, 9], [6, 8]
+    );
+    realpos = new Array(
+        [0], [0, 0], [150, 0], [300, 0], [0, 150], [150, 150], [300, 150], [0, 300], [150, 300], [300, 300]
+    );
+    for (let i = 1; i <= 8; ++i) d[i] = i; d[9] = 0;
+    for (let i = 1; i <= 100; ++i) {
+        var x = parseInt(Math.random() * 8 + 1);
+        var y = parseInt(Math.random() * 8 + 1);
+        if (x == y) {i--; continue;}
+        if (d[x] != 0) {
+            document.getElementById("d" + d[x]).style.left = realpos[y][0] + "px";
+            document.getElementById("d" + d[x]).style.top = realpos[y][1] + "px";
+        }
+        if (d[y] != 0) {
+            document.getElementById("d" + d[y]).style.left = realpos[x][0] + "px";
+            document.getElementById("d" + d[y]).style.top = realpos[x][1] + "px";
+        }
+        d[x] ^= d[y];
+        d[y] ^= d[x];
+        d[x] ^= d[y];
+    }
+}
---
+++ 别踩白块/js/index.js
@@ -0,0 +1,103 @@
+﻿let flag = false;
+let clock = null;
+let speed = 6;
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function createDiv(name) {
+    let div = document.createElement("div");
+    div.className = name;
+    return div;
+}
+
+function createCell() {
+    let temp = ["white_cell", "white_cell", "white_cell", "white_cell"];
+    let position = Math.floor(Math.random() * 4);
+    temp[position] = "black_cell";
+    return temp;
+}
+
+function createRow() {
+    let con = $("con");
+    let row = createDiv("row");
+    let arr = createCell();
+    for (let i = 0; i < 4; ++i) {
+        let cell = createDiv(arr[i]);
+        row.appendChild(cell);
+    }
+    if (con.firstChild == null) con.appendChild(row);
+    else                        con.insertBefore(row, con.firstChild);
+}
+
+function deleteRow() {
+    let con = $("con");
+    if (con.childNodes.length == 6) {
+        con.removeChild(con.lastChild);
+    }
+}
+
+function start() {
+    if (!flag) init();
+    else alert("游戏已经开始!");
+}
+
+function init() {
+    flag = true;
+    for (let i = 0; i < 4; ++i) createRow();
+    $("main").onclick = function (ev) {
+        ev = ev || event;
+        judge(ev);
+    }
+    clock = window.setInterval("move()", 30);
+}
+
+function judge(ev) {
+    if (ev.target.className == "black_cell") {
+        ev.target.className = "white_cell";
+        ev.target.parentNode.pass = 200;//黑块被点击
+        score();
+    }
+    else if (ev.target.className == "white_cell") {
+        ev.target.parentNode.pass = 404;//白块被点击
+    }
+}
+
+function checkState() {
+    let con = $("con");
+    let rows = con.childNodes;
+    if (rows.length == 5 && rows[rows.length - 1].pass != 200) gameOver();
+    for (let i = 0; i < rows.length; ++i) {
+        if (rows[i].pass == 404) gameOver();
+    }
+}
+
+function score() {
+    let newScore = parseInt($("score").innerHTML) + 1;
+    $("score").innerHTML = newScore;
+}
+
+function move() {
+    let con = $("con");
+    let top = parseInt(window.getComputedStyle(con, null)["top"]);
+    if (top + speed > 0) top = 0;
+    else top += speed;
+    con.style.top = top + "px";
+    checkState();
+    if (top == 0) {
+        createRow();
+        con.style.top = "-102px";
+        deleteRow();
+    }
+}
+
+function gameOver() {
+    clearInterval(clock);
+    flag = false;
+    confirm("你的最终得分为" + $("score").innerHTML);
+    var con = $("con");
+    con.innerHTML = "";
+    $("score").innerHTML = 0;
+    con.style.top = "-408px";
+}
