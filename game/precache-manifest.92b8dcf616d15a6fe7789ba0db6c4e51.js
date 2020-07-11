d857b7c175804bde91815b7bec27d2e03c95561a
songyiqun <594207613@qq.com>
2020-07-11 20:19:12 +0800

    20200711

---
+++ game/precache-manifest.92b8dcf616d15a6fe7789ba0db6c4e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dace4e310e5be959735d5e65c679d9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7e8e844a7ebd62b141c",
+    "url": "./static/css/main.6e796cec.chunk.css"
+  },
+  {
+    "revision": "3d28800c2a336a203898",
+    "url": "./static/js/2.122e103d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.122e103d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7e8e844a7ebd62b141c",
+    "url": "./static/js/main.34ba00b4.chunk.js"
+  },
+  {
+    "revision": "85f8b4203e047e2647f2",
+    "url": "./static/js/runtime-main.cb5fa8b2.js"
+  }
+]);
